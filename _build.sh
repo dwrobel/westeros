7ba8bf97e7b0ba17422c812707ac2fb2944f0ea2
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2017-10-11 15:38:51 +0200

    Add helper scripts
    Change-Id: I38b8b3874559a50167f80241cee7e5a52ea357cc

---
+++ _build.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -ex
+#
+# Simple build script
+# Author: Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
+#
+# Fedora dependencies:
+# dnf install make autoconf automake libtool glib2-devel wayland-devel mesa-libgbm-devel egl-wayland-devel libdrm-devel libglvnd-devel libxkbcommon-devel gstreamer1-devel mesa-dri-drivers gcc-c++
+#
+
+export OUTDIR=$PWD/out
+
+export LT_SYS_LIBRARY_PATH=${OUTDIR}/lib
+export LDFLAGS=${LT_SYS_LIBRARY_PATH}
+
+export SCANNER_TOOL=wayland-scanner
+
+export CFLAGS="-O0 -g3 -I${OUTDIR}/include"
+export CXXFLAGS="${CFLAGS}"
+export LDFLAGS="${CFLAGS} -L${OUTDIR}/lib"
+
+for dir in simpleshell simplebuffer .; do
+   make -C ${dir}/protocol
+done
+
+pushd essos
+  autoreconf -sif; ./configure --enable-essoswesterosfree --prefix=${OUTDIR}; make -j$(getconf _NPROCESSORS_ONLN) V=1 install
+popd
+
+for dir in simpleshell simplebuffer drm; do
+  pushd ${dir}
+    autoreconf -sif; ./configure --prefix=${OUTDIR}; make -j$(getconf _NPROCESSORS_ONLN) V=1 install
+  popd
+done
+
+autoreconf -sif
+
+./configure \
+    --prefix=${OUTDIR} \
+    --enable-rendergl \
+    --enable-sbprotocol \
+    --enable-app \
+    --disable-xdgv4 \
+    --disable-xdgv5 \
+    --enable-xdgstable \
+    --enable-test \
+    --enable-modules \
+    
+
+make -j$(getconf _NPROCESSORS_ONLN) V=1 install
